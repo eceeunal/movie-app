2de3e33b2fd5ed38a2507b39ec2dc76d4b6bf3ff
eceeunal <ecedogann3@gmail.com>
2024-05-02 01:37:25 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Register from "./pages/Register";
+import AppRouter from "./router/AppRouter";
+import AuthProvider from "./context/AuthProvider";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dark:bg-gray-dark-main min-h-screen">
+      <AuthProvider>
+      <AppRouter />
+      </AuthProvider>
+      
     </div>
   );
-}
+};
 
 export default App;
